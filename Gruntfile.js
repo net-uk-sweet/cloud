4971a20d233364cd1ca492fcaf16bb55b69d9eee
net-uk-sweet <ian@sweet.uk.net>
2015-03-02 09:46:39 +0000

    Removed unnecessary jquery dependency (can get away with jq lite. Added first-pass at copy, inject, annotate and template tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,14 @@ module.exports = function(grunt) {
 				dest: 'dist/css/style.css'
 			}
 		},
+		ngAnnotate: {
+			options: { singleQuotes: true },
+			app: {
+				files: {
+					'dist/js/app.js': ['dist/js/app.js']
+				}
+			}
+		},
 		uglify: {
 			build: {
 				src: 'dist/js/app.js',
@@ -31,14 +39,50 @@ module.exports = function(grunt) {
 					dest: 'dist/css'
 				}]
 			}
+		},
+		copy: {
+			main: {
+				files: [
+					{ expand: true, src: 'index.html', dest: 'dist/' },
+					{ expand: true, src: 'font', dest: 'dist/' }
+				]
+			}
+		},
+		ng_template: {
+			files: ['js/partials'],
+			options: {
+				appDir: 'dist',
+				indexFile: 'index.html',
+				concat: true
+			}
+		},
+		injector: {
+			options: {},
+			localDependencies: {
+				files: {
+					'dist/index.html': ['dist/js/app.js', 'dist/css/style.css']
+				}
+			}
 		}
 	});
 
 	// Import plugins
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-ng-annotate');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-ng-template');
+	grunt.loadNpmTasks('grunt-injector');
 
 	// Configure our tasks
-	grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+	grunt.registerTask('default', [
+		'concat', 
+		'ngAnnotate', 
+		'uglify', 
+		'cssmin', 
+		'copy', 
+		'ng_template',
+		'injector'
+	]);
 };
